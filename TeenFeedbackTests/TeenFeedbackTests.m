e668694b717fb2203b7b413a147cb256f5fc130e
Lukas Marx and Serguei Filimonov <pair+lmarks+sfilimonov@pivotallabs.com>
2014-02-19 10:05:37 -0800

    Initial Commit

---
+++ TeenFeedbackTests/TeenFeedbackTests.m
@@ -0,0 +1,34 @@
+//
+//  TeenFeedbackTests.m
+//  TeenFeedbackTests
+//
+//  Created by pivotal on 2/19/14.
+//  Copyright (c) 2014 PivotalBeach. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TeenFeedbackTests : XCTestCase
+
+@end
+
+@implementation TeenFeedbackTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
